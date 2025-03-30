b5017a50bb282b9f53b43405572c00950e5637e0
LunaRenilson <renilson.c.luna@gmail.com>
2025-03-30 19:43:24 -0300

    lab finalizado

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,45 +1,54 @@
-#include <stdio.h>
-
-void le_entrada(int *valores)
-{
-     for (int i = 0; i < 8; i++)
-     {
-          char entrada[5];
-          // int n = read(STDIN_FD, entrada, 5);
-          scanf("%s", entrada);
-
-          int sinal = (entrada[0] == '+' ? 1 : -1);
-
-          valores[i] += (entrada[1] - 48) * 1000; // x000
-          valores[i] += (entrada[2] - 48) * 100;  // 0x00
-          valores[i] += (entrada[3] - 48) * 10;   // 00x0
-          valores[i] += (entrada[4] - 48);        // 000x
-
-          // Transformando em positivo ou negativo
-          valores[i] *= sinal;
-     }
-
-     for (int i = 0; i < 8; i++)
-     {
-          printf("%d ", valores[i]);
-     }
-}
-
-int organizarBits(int *valores)
-{
-     unsigned int N1 = valores[0] & valores[1];    // 1 AND 2 
-     unsigned int N2 = valores[2] | valores[3];    // 3 OR 4 
-     unsigned int N3 = valores[4] ^ valores[5];    // 5 XOR 6
-     unsigned int N4 = ~(valores[6] & valores[7]); // 7 NAND 8 (NAND é o inverso do AND)
-
-     int resultado = (N1 & 0xFF)          // bits 0-7
-                     | (N2 & 0xFF) << 8   // bits 8-15
-                     | (N3 & 0xFF) << 16  // bits 16-23
-                     | (N4 & 0xFF) << 24; // bits 24-31;
-
-     printf("%d\n", N4);
-     return resultado;
-}
+int read(int __fd, const void *__buf, int __n){
+     int ret_val;
+   __asm__ __volatile__(
+     "mv a0, %1           # file descriptor\n"
+     "mv a1, %2           # buffer \n"
+     "mv a2, %3           # size \n"
+     "li a7, 63           # syscall write code (63) \n"
+     "ecall               # invoke syscall \n"
+     "mv %0, a0           # move return value to ret_val\n"
+     : "=r"(ret_val)  // Output list
+     : "r"(__fd), "r"(__buf), "r"(__n)    // Input list
+     : "a0", "a1", "a2", "a7"
+   );
+   return ret_val;
+ }
+ 
+ void write(int __fd, const void *__buf, int __n)
+ {
+   __asm__ __volatile__(
+     "mv a0, %0           # file descriptor\n"
+     "mv a1, %1           # buffer \n"
+     "mv a2, %2           # size \n"
+     "li a7, 64           # syscall write (64) \n"
+     "ecall"
+     :   // Output list
+     :"r"(__fd), "r"(__buf), "r"(__n)    // Input list
+     : "a0", "a1", "a2", "a7"
+   );
+ }
+ 
+ void exit(int code)
+ {
+   __asm__ __volatile__(
+     "mv a0, %0           # return code\n"
+     "li a7, 93           # syscall exit (64) \n"
+     "ecall"
+     :   // Output list
+     :"r"(code)    // Input list
+     : "a0", "a7"
+   );
+ }
+ 
+ void _start()
+ {
+   int ret_code = main();
+   exit(ret_code);
+ }
+ 
+ #define STDIN_FD  0
+ #define STDOUT_FD 1
+ 
 
 void hex_code(int val)
 {
@@ -59,9 +68,43 @@ void hex_code(int val)
                hex[i] = aux + '0';
           uval = uval / 16;
      }
-     // write(1, hex, 11);
-     printf("%s\n", hex);
+     write(1, hex, 11);
 }
+
+void le_entrada(int *valores)
+{
+     for (int i = 0; i < 8; i++)
+     {
+          char entrada[6];
+          int n = read(STDIN_FD, entrada, 6);
+          int valor =  (entrada[1] - 48) * 1000 // x000
+                    + (entrada[2] - 48) * 100  // 0x00
+                    + (entrada[3] - 48) * 10   // 00x0
+                    + (entrada[4] - 48);        // 000x
+
+          // Transformando em positivo ou negativo
+          if (entrada[0] == '-'){
+               valor = -valor;
+          }
+
+          valores[i] = valor;
+     }
+}
+
+int organizarBits(int *valores)
+{
+     unsigned int N2 = valores[2] | valores[3];    // 3 OR 4 
+     unsigned int N1 = valores[0] & valores[1];    // 1 AND 2 
+     unsigned int N3 = valores[4] ^ valores[5];    // 5 XOR 6
+     unsigned int N4 = ~(valores[6] & valores[7]); // 7 NAND 8 (NAND é o inverso do AND)
+
+     unsigned int resultado = (N1 & 0xFF)           // N1: bits 0-7 (LSB)
+               | ((N2 & 0xFF) << 8 )      // N2: bits 8-15 (LSB)
+               | ((N4 >> 16) & 0xFF) << 16  // N4: MSB (16-23)
+               | ((N3 >> 24) & 0xFF) << 24; // N3: MSB (24-31) 
+     return resultado;
+}
+
 
 int main()
 {
