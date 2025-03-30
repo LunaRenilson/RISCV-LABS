64fb19470bde14150c4e5cf80320f84f456b4d95
LunaRenilson <renilson.c.luna@gmail.com>
2025-03-30 12:28:04 -0300

    erro no nand

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,3 +1,46 @@
+#include <stdio.h>
+
+void le_entrada(int *valores)
+{
+     for (int i = 0; i < 8; i++)
+     {
+          char entrada[5];
+          // int n = read(STDIN_FD, entrada, 5);
+          scanf("%s", entrada);
+
+          int sinal = (entrada[0] == '+' ? 1 : -1);
+
+          valores[i] += (entrada[1] - 48) * 1000; // x000
+          valores[i] += (entrada[2] - 48) * 100;  // 0x00
+          valores[i] += (entrada[3] - 48) * 10;   // 00x0
+          valores[i] += (entrada[4] - 48);        // 000x
+
+          // Transformando em positivo ou negativo
+          valores[i] *= sinal;
+     }
+
+     for (int i = 0; i < 8; i++)
+     {
+          printf("%d ", valores[i]);
+     }
+}
+
+int organizarBits(int *valores)
+{
+     unsigned int N1 = valores[0] & valores[1];    // 1 AND 2 
+     unsigned int N2 = valores[2] | valores[3];    // 3 OR 4 
+     unsigned int N3 = valores[4] ^ valores[5];    // 5 XOR 6
+     unsigned int N4 = ~(valores[6] & valores[7]); // 7 NAND 8 (NAND é o inverso do AND)
+
+     int resultado = (N1 & 0xFF)          // bits 0-7
+                     | (N2 & 0xFF) << 8   // bits 8-15
+                     | (N3 & 0xFF) << 16  // bits 16-23
+                     | (N4 & 0xFF) << 24; // bits 24-31;
+
+     printf("%d\n", N4);
+     return resultado;
+}
+
 void hex_code(int val)
 {
      char hex[11];
@@ -16,78 +59,17 @@ void hex_code(int val)
                hex[i] = aux + '0';
           uval = uval / 16;
      }
-     write(1, hex, 11);
+     // write(1, hex, 11);
+     printf("%s\n", hex);
 }
 
-int read(int __fd, const void *__buf, int __n)
+int main()
 {
-     int ret_val;
-     __asm__ __volatile__(
-         "mv a0, %1           # file descriptor\n"
-         "mv a1, %2           # buffer \n"
-         "mv a2, %3           # size \n"
-         "li a7, 63           # syscall write code (63) \n"
-         "ecall               # invoke syscall \n"
-         "mv %0, a0           # move return value to ret_val\n"
-         : "=r"(ret_val)                   // Output list
-         : "r"(__fd), "r"(__buf), "r"(__n) // Input list
-         : "a0", "a1", "a2", "a7");
-     return ret_val;
-}
 
-void write(int __fd, const void *__buf, int __n)
-{
-     __asm__ __volatile__(
-         "mv a0, %0           # file descriptor\n"
-         "mv a1, %1           # buffer \n"
-         "mv a2, %2           # size \n"
-         "li a7, 64           # syscall write (64) \n"
-         "ecall"
-         :                                 // Output list
-         : "r"(__fd), "r"(__buf), "r"(__n) // Input list
-         : "a0", "a1", "a2", "a7");
-}
-
-void exit(int code)
-{
-     __asm__ __volatile__(
-         "mv a0, %0           # return code\n"
-         "li a7, 93           # syscall exit (64) \n"
-         "ecall"
-         :           // Output list
-         : "r"(code) // Input list
-         : "a0", "a7");
-}
-
-void _start()
-{
-     int ret_code = main();
-     exit(ret_code);
-}
-
-#define STDIN_FD 0
-#define STDOUT_FD 1
-
-void le_entrada(char * entrada, int * numeros) {
-     for (int i = 0; i < 47; i++){
-          char entrada[5];
-          int sinal;
-          int n = read(STDIN_FD, entrada, 5);
-
-          sinal = (entrada[0] == '+' ? 1 : -1);
-
-          numeros[0] = 0; // incializando valor
-          numeros[0] += (entrada[1] - '48') * 1000;
-          numeros[0] += (entrada[1] - '48') * 100;
-          numeros[0] += (entrada[1] - '48') * 10;
-          numeros[0] += (entrada[1] - '48');
-          
-     }
-}
-
-int main() {
-
-     int numeros[8];
+     int valores[8] = {0};
+     le_entrada(valores);
+     int total = organizarBits(valores);
+     hex_code(total);
 
      return 0;
 }
