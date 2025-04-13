3e4f76cd6bc2bcd9d55207a1fef0419534fbbe7c
LunaRenilson <renilson.c.luna@gmail.com>
2025-04-13 18:36:58 -0300

    itoa completo (int to str)

--- lab6/lab06.s
+++ lab6/lab06.s
@@ -10,15 +10,73 @@ _start:
 # Entrada: "5\n"
 main: 
      
-     # li a2, 4
-     # jal read
-
-     la a0, test_string
+     li a2, 4
+     jal read
+     
+     la a0, input
      li a1, 32
      jal atoi
 
+     la a1, resultado
+     jal itoa
+
+     jal write
      j exit
 
+
+# Converte int em str
+# Entrada: a0 (int), a1(buffer de saída)
+itoa: 
+
+
+     addi sp, sp, -32         # reserva espaço na pilha (ajustável)
+     mv t0, sp
+     li t1, 0                 # qtd de caracteres (contador)  
+     li t2, 10                # divisor   
+
+     # Trata caso especial de zero
+     beqz a0, trata_zero
+
+
+     loop_itoa:
+          beqz a0, desempilha_itoa
+          rem t3, a0, t2      # Obtendo digito menos significativo
+          div a0, a0, t2      # Deslocando inteiro
+          addi t3, t3, 48     # Convertendo pra char
+          sb t3, 0(t0)        # Armazenando valor convertido
+          
+          addi t0, t0, 1      # Andando na pilhar
+          addi t1, t1, 1      # Incrementando contador
+
+          j loop_itoa
+     
+     desempilha_itoa:
+          beqz t1, fim_itoa
+          lb t3, -1(t0)        # Desempilhando char
+          sb t3, 0(a1)        # empilhando na variavel da saida
+
+          addi t0, t0, -1     # Voltando na pilha aux
+          addi a1, a1, 1      # Avançando na pilha de saida
+          addi t1, t1, -1
+          j desempilha_itoa
+
+
+     fim_itoa:
+          sb t2, 0(a1)        # Adicionando terminador
+          # Restaurando a pilha
+          addi sp, sp, 32     
+
+          ret
+
+     trata_zero:
+          li t3, 48           # '0'
+          sb t3, 0(t0)        # carregando valor na pilha
+          addi t0, t0, 1      # andando na pilha
+          li t1, 1            # Contador de char (1, nesse caso)
+          j desempilha_itoa   # Desempilhando
+
+
+# Converte str em int
 # Entrada: a0 (string) a1 (terminador)
 atoi:
      lb t0, 0(a0)        # Primeiro char
@@ -37,6 +95,7 @@ atoi:
           mv a0, t1
           ret
 
+# entrada: a2 (tamanho do buffer a ser lido)
 read:
      li a0, 0             # file descriptor = 0 (stdin)
      la a1, input # buffer
@@ -48,7 +107,7 @@ read:
 write:
      li a0, 1            # file descriptor = 1 (stdout)
      la a1, resultado       # buffer
-     li a2, 4            # size - Writes 4 bytes.
+     li a2, 3            # size - Writes 4 bytes.
      li a7, 64           # syscall write (64)
      ecall
      ret
